733a5bc65ac6d46716c6c7fb3e57e50e4c70d4b7
Kwan Lam <kwanlam@kwans-macbook-pro.local>
2018-05-12 20:39:52 -0700

    Test

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include <math.h>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cout << 1 + 1 << endl;
+    
     return 0;
 }
